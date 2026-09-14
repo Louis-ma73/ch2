ba027535dfd6ea6d4f047121832a47ec46da4b22
Louis-ma73 <2735964@learn.vsb.bc.ca>
2026-09-14 14:51:51 -0700

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,27 @@
+
+public class Example1{
+    public static void main(String[] args){
+    //Practice
+    String name;
+    name = "Louis";
+    int age = 16;
+    
+    //Ex 2.2
+    int day = 14;
+    String month;
+    month = "Sepetmber";
+    int year = 2026;
+   
+    //Ex 2.3
+    
+    //Practice
+    System.out.println("My name is" +" " + name + ", " + "and I am " + " " + age);
+    System.out.println(7/2);
+    System.out.println(7.0/2);
+    System.out.println(7%2);
+    System.out.println("Today is" + " " + day + ", " + month + ", " + year);
+    
+    }
+}
+    
+    
