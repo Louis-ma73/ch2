ba4feed3a761cda659f62d798fcac37a3799ea1b
Louis-ma73 <2735964@learn.vsb.bc.ca>
2026-09-15 14:16:13 -0700

    Update Example1.java
    I did a little bit change for the original copy

--- Example1.java
+++ Example1.java
@@ -13,7 +13,17 @@ public class Example1{
     int year = 2026;
    
     //Ex 2.3
-    
+    int hour = 14;
+    int minute = 50;
+    int second;
+    second = minute *60 + hour * 3600;
+    System.out.println("Current time is:");
+    System.out.print(hour);
+    System.out.print(":");
+    System.out.print(minute);
+    System.out.print("");
+    System.out.println(" It has been" + " " + second + " " + "Second since midnight");
+        
     //Practice
     System.out.println("My name is" +" " + name + ", " + "and I am " + " " + age);
     System.out.println(7/2);
